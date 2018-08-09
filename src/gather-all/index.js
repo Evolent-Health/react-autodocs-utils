41db9921394e4dc77021a6525bfd134663f580e4
Marwane Boudriga <mboudriga@evolenthealth.com>
2018-08-09 15:13:36 -0400

    changing readme path

--- src/gather-all/index.js
+++ src/gather-all/index.js
@@ -52,13 +52,8 @@ const gatherAll = path =>
           .then(file => readFile(pathJoin(dirname(path), file)))
           .then(({source}) => source)
           .catch(() => Promise.resolve(''));
-
-        
-      console.log(metadata.displayName);
-      console.log(readMarkdown('./docs/readme.md'));
-      console.log(readMarkdown('./docs/' + metadata.displayName + '.md'));
       
-      const readme = readMarkdown('./docs/' + metadata.displayName + '.md');
+      const readme = readMarkdown('docs/' + metadata.displayName + '.md');
       const readmeAccessibility = readMarkdown('readme.accessibility.md');
       const readmeTestkit = readMarkdown('readme.testkit.md');
 
